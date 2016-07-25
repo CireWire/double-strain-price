0b12f1899175b338989b3b8082ff076fe099ff26
CireWire <djcire956@gmail.com>
2016-07-24 23:42:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+strains = {
+	"Northern Lights" : 25.00,
+	"Big Bud" : 12.00,
+	"Skunk" : 23.00,
+	"OG Kush" : 18.00
+	}
+	
+for key in strains:
+	total = 0
+	total = total + strains[key] * 2
+	print total
